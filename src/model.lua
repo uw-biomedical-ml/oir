8edb4e6781f4eaef9b38c01b46cbc8a870796139
saerxiao <sa.xiao2009@gmail.com>
2017-03-27 21:16:22 -0700

    unet wip

---
+++ src/model.lua
@@ -0,0 +1,91 @@
+local model = {}
+
+function model.uNetNOCopy(opt)
+  local input = - nn.Identity()
+  -- contracting path
+  local c1 = input
+             - nn.SpatialConvolutionMM(1,64,3,3,1,1,1,1) -- 64 channels, (patchSize-3+1*2)/1+1 = patchSize, output 64 x patchSize x patchSize
+             - nn.ReLU()
+             - nn.SpatialConvolutionMM(64,64,3,3,1,1,1,1) -- 64 channels, output 64 x patchSize x patchSize
+             - nn.ReLU()
+  local c2 = c1
+             - nn.SpatialMaxPooling(2,2)  -- output: 64 x patchSize/2 x patchSize/2
+             - nn.SpatialConvolutionMM(64,128,3,3,1,1,1,1)  -- 128 channels, output 128 x patchSize/2 x patchSize/2
+             - nn.ReLU()
+             - nn.SpatialConvolutionMM(128,128,3,3,1,1,1,1)  -- 128 channels, output 128 x patchSize/2 x patchSize/2
+             - nn.ReLU()
+  local c3 = c2
+             - nn.SpatialMaxPooling(2,2)  -- output: 128 x patchSize/4 x patchSize/4
+             - nn.SpatialConvolutionMM(128,256,3,3,1,1,1,1)  -- 256 channels, output 256 x patchSize/4 x patchSize/4
+             - nn.ReLU()
+             - nn.SpatialConvolutionMM(256,256,3,3,1,1,1,1)  -- 256 channels
+             - nn.ReLU()
+             - nn.SpatialUpSamplingNearest(2)  -- output: 256 x patchSize/2 x patchSize/2
+  -- expansive path
+  local c2Mirror = c3
+                   - nn.SpatialConvolutionMM(256,128,3,3,1,1,1,1)  -- 128 filters, output 128 x patchSize/2 x patchSize/2
+                   - nn.ReLU()
+                   - nn.SpatialConvolutionMM(128,128,3,3,1,1,1,1)  -- 128 filters, output 128 x patchSize/2 x patchSize/2
+                   - nn.ReLU()
+                   - nn.SpatialMaxPooling(2,2) -- output: 128 x patchSize x patchSize
+  local c1Mirror = c2Mirror
+                   - nn.SpatialConvolutionMM(128,64,3,3,1,1,1,1)  -- 64 filters, output 64 x patchSize x patchSize
+                   - nn.ReLU()
+                   - nn.SpatialConvolutionMM(64,64,3,3,1,1,1,1)  -- output 64 x patchSize x patchSize
+                   - nn.ReLU()
+  -- make the right shape as the input
+  local last = c1Mirror
+               - nn.SpatialConvolutionMM(64,opt.nClasses,1,1,1,1,0,0) -- (patchSize-1)/1+1 = patchSize, output nClasses x patchSize x patchSize
+               - nn.Reshape(opt.nClasses, -1)  -- output batchsize x nClasses x (number of pixels)
+               - nn.Transpose({2,3})  -- output batchsize x (number of pixels) x nClasses
+               - nn.Reshape(-1, opt.nClasses, false) -- output (batchsize* number of pixels) x nClasses
+  local g = nn.gModule({input},{last})
+  return g
+end
+
+function model.uNet(opt)
+  local input = - nn.Identity()
+  -- contracting path
+  local c1 = input
+             - nn.SpatialConvolutionMM(1,64,3,3,1,1,1,1) -- 64 channels, (patchSize-3+1*2)/1+1 = patchSize, output 64 x patchSize x patchSize
+             - nn.ReLU()
+             - nn.SpatialConvolutionMM(64,64,3,3,1,1,1,1) -- 64 channels, output 64 x patchSize x patchSize
+             - nn.ReLU()
+  local c2 = c1
+             - nn.SpatialMaxPooling(2,2)  -- output: 64 x patchSize/2 x patchSize/2
+             - nn.SpatialConvolutionMM(64,128,3,3,1,1,1,1)  -- 128 channels, output 128 x patchSize/2 x patchSize/2
+             - nn.ReLU()
+             - nn.SpatialConvolutionMM(128,128,3,3,1,1,1,1)  -- 128 channels, output 128 x patchSize/2 x patchSize/2
+             - nn.ReLU()
+  local c3 = c2
+             - nn.SpatialMaxPooling(2,2)  -- output: 128 x patchSize/4 x patchSize/4
+             - nn.SpatialConvolutionMM(128,256,3,3,1,1,1,1)  -- 256 channels, output 256 x patchSize/4 x patchSize/4
+             - nn.ReLU()
+             - nn.SpatialConvolutionMM(256,256,3,3,1,1,1,1)  -- 256 channels
+             - nn.ReLU()
+             - nn.SpatialUpSamplingNearest(2)  -- output: 256 x patchSize/2 x patchSize/2
+  -- expansive path
+  local c2Mirror = {c3, c2}
+                   - nn.JoinTable(2) -- output: (256+128) channels x patchSize/2 x patchSize/2
+                   - nn.SpatialConvolutionMM(256+128,128,3,3,1,1,1,1)  -- 128 filters, output 128 x patchSize/2 x patchSize/2
+                   - nn.ReLU()
+                   - nn.SpatialConvolutionMM(128,128,3,3,1,1,1,1)  -- 128 filters, output 128 x patchSize/2 x patchSize/2
+                   - nn.ReLU()
+                   - nn.SpatialMaxPooling(2,2) -- output: 128 x patchSize x patchSize
+  local c1Mirror = {c2Mirror, c1}
+                   - nn.JoinTable(2) -- output: (128+64) channels x patchSize x patchSize
+                   - nn.SpatialConvolutionMM(128+64,64,3,3,1,1,1,1)  -- 64 filters, output 64 x patchSize x patchSize
+                   - nn.ReLU()
+                   - nn.SpatialConvolutionMM(64,64,3,3,1,1,1,1)  -- output 64 x patchSize x patchSize
+                   - nn.ReLU()
+  -- make the right shape as the input
+  local last = c1Mirror
+               - nn.SpatialConvolutionMM(64,opt.nClasses,1,1,1,1,0,0) -- (patchSize-1)/1+1 = patchSize, output nClasses x patchSize x patchSize
+               - nn.Reshape(opt.nClasses, -1)  -- output batchsize x nClasses x (number of pixels)
+               - nn.Transpose({2,3})  -- output batchsize x (number of pixels) x nClasses
+               - nn.Reshape(-1, opt.nClasses, false) -- output (batchsize* number of pixels) x nClasses
+  local g = nn.gModule({input},{last})
+  return g
+end
+
+return model
--- src/train.lua
+++ src/train.lua
@@ -2,8 +2,11 @@ require 'torch'
 require 'nn'
 require 'optim'
 
+local model = require 'model'
+
 local cmd = torch.CmdLine()
-cmd:option('--dir', '/Users/saxiao/AI/oir/data/', 'data directory')
+cmd:option('--dir', '/home/saxiao/eclipse/workspace/oir/data/', 'data directory')
+cmd:option('--nClasses', 2, 'number of classes')
 cmd:option('--trainSize', 0.5, 'training set percentage')
 cmd:option('--validateSize', 0.5, 'validate set percentage')
 cmd:option('--batchSize', 10, 'batch size')
@@ -11,51 +14,49 @@ cmd:option('--patchSize', 64, 'patch size')
 cmd:option('--spacing', -1, 'spacing between each patch, -1 means no overlapping, so same as the patchSize')
 
 -- training options
-cmd:option('--nIterations', 100, 'patch size')
+cmd:option('--nIterations', 3000, 'patch size')
 cmd:option('--learningRate', 1e-3, 'patch size')
 cmd:option('--momentum', 0.9, 'patch size')
 
-local nClasses = 2
+-- gpu options
+cmd:option('--gpuid', 0, 'patch size')
+cmd:option('--seed', 123, 'patch size')
+
+-- checkpoint options
+cmd:option('--plotDir', '/home/saxiao/eclipse/workspace/oir/plot/', 'plot directory')
 
 local opt = cmd:parse(arg)
+
+-- load lib for gpu
+if opt.gpuid > -1 then
+  local ok, cunn = pcall(require, 'cunn')
+  local ok2, cutorch = pcall(require, 'cutorch')
+  if not ok then print('package cunn not found!') end
+  if not ok2 then print('package cutorch not found!') end
+  if ok and ok2 then
+    print('using CUDA on GPU ' .. opt.gpuid .. '...')
+    cutorch.setDevice(opt.gpuid + 1) -- note +1 to make it 0 indexed! sigh lua
+    cutorch.manualSeed(opt.seed)
+  else
+    print('If cutorch and cunn are installed, your CUDA toolkit may be improperly configured.')
+    print('Check your CUDA toolkit installation, rebuild cutorch and cunn, and try again.')
+    print('Falling back on CPU mode')
+    opt.gpuid = -1 -- overwrite user setting
+  end
+end
 
 local Loader = require 'Loader'
 local loader = Loader.create(opt)
 
-local net = nn.Sequential()
--- contracting path
-net:add(nn.SpatialConvolutionMM(1,64,3,3,1,1,1,1)) -- 64 filters, (patchSize-3+1*2)/1+1 = patchSize, output 64 x patchSize x patchSize
-net:add(nn.ReLU())
-net:add(nn.SpatialConvolutionMM(64,64,3,3,1,1,1,1)) -- 64 filters, output 64 x patchSize x patchSize
-net:add(nn.ReLU())
-net:add(nn.SpatialMaxPooling(2,2)) -- output: 64 x patchSize/2 x patchSize/2
-net:add(nn.SpatialConvolutionMM(64,128,3,3,1,1,1,1)) -- 128 filters, output 128 x patchSize/2 x patchSize/2
-net:add(nn.ReLU())
-net:add(nn.SpatialConvolutionMM(128,128,3,3,1,1,1,1)) -- 128 filters, output 128 x patchSize/2 x patchSize/2
-net:add(nn.ReLU())
-net:add(nn.SpatialMaxPooling(2,2)) -- output: 128 x patchSize/4 x patchSize/4
-net:add(nn.SpatialConvolutionMM(128,256,3,3,1,1,1,1)) -- 256 filters, output 256 x patchSize/4 x patchSize/4
-net:add(nn.ReLU())
-net:add(nn.SpatialConvolutionMM(256,256,3,3,1,1,1,1)) -- 256 filters
-net:add(nn.ReLU())
--- expansive path
-net:add(nn.SpatialUpSamplingNearest(2)) -- output: 256 x patchSize/2 x patchSize/2
-net:add(nn.SpatialConvolutionMM(256,128,3,3,1,1,1,1)) -- 128 filters, output 128 x patchSize/2 x patchSize/2
-net:add(nn.ReLU())
-net:add(nn.SpatialConvolutionMM(128,128,3,3,1,1,1,1))
-net:add(nn.ReLU())
-net:add(nn.SpatialUpSamplingNearest(2)) -- output: 128 x patchSize x patchSize
-net:add(nn.SpatialConvolutionMM(128,64,3,3,1,1,1,1)) -- 64 filters, output 64 x patchSize x patchSize
-net:add(nn.ReLU())
-net:add(nn.SpatialConvolutionMM(64,64,3,3,1,1,1,1)) -- 64 filters 
-net:add(nn.ReLU())
--- make the right shape for class prediction
-net:add(nn.SpatialConvolutionMM(64,nClasses,1,1,1,1,0,0)) -- (patchSize-1)/1+1 = patchSize, output nClasses x patchSize x patchSize
-net:add(nn.Reshape(nClasses, -1)) -- output batchsize x nClasses x (number of pixels)
-net:add(nn.Transpose({2,3})) -- output batchsize x (number of pixels) x nClasses
-net:add(nn.Reshape(-1, nClasses, false)) -- output (batchsize* number of pixels) x nClasses
+local net = model.uNet(opt)
 
 local criterion = nn.CrossEntropyCriterion()
+
+-- ship the model to the GPU if desired
+if opt.gpuid == 0 then
+  net = net:cuda()
+  criterion = criterion:cuda()
+end
 
 local params, grads = net:getParameters()
 local trainIter = loader:iterator("train")
@@ -82,6 +83,8 @@ local function diceCoef(output, target)
 end
 
 local trainAccuracy = nil
+local trainAccuracyHistory = {}
+local validateAccuracyHistory = {}
 
 local feval = function(w)
   if w ~= params then
@@ -90,8 +93,11 @@ local feval = function(w)
   grads:zero()
 
   local data, label = trainIter.nextBatch()
+  data = data:type(type)
+  label = label:type(type)
   local predicted = net:forward(data)
   trainAccuracy = accuracy(predicted, label)
+  table.insert(trainAccuracyHistory, trainAccuracy)
   local loss = criterion:forward(predicted, label)
   local gradScore = criterion:backward(predicted, label)
   net:backward(data, gradScore)
@@ -102,13 +108,23 @@ end
 local validateIter = loader:iterator("validate")
 local function validateAccuracy()
   local data, label = validateIter.nextBatch()
+  data = data:type(type)
+  label = label:type(type)
   local output = net:forward(data)
   return accuracy(output, label)
 end
 
+local validateAccuracyHistory = {}
 local optimOpt = {learningRate = opt.learningRate, momentum = opt.momentum}
 for i = 1, opt.nIterations do
   local _, loss = optim.adagrad(feval, params, optimOpt)
-  print("loss=", loss[1], " trainAccuracy=", trainAccuracy, " validateAccuracy=", validateAccuracy())
+  local valudateAccuracy = validateAccuracy()
+  print("i=", i, " loss=", loss[1], " trainAccuracy=", trainAccuracy, " validateAccuracy=", valudateAccuracy)
+  table.insert(validateAccuracyHistory, valudateAccuracy)
 end
 
+local trainHistory = {}
+trainHistory.trainAccuracy = trainAccuracyHistory
+trainHistory.validateAccuracy = validateAccuracyHistory
+local trainHistoryFile = opt.plotDir .. "trainHistory.t7"
+torch.save(trainHistoryFile, trainHistory)
